97cc03ea565f6e892751482fbe79dc1bb1d1c60f
albornzineCahill <legojonah39@gmail.com>
2014-09-16 19:37:07 -0500

    i finished the questions

--- programs/functionLibraryAutoProgramQuestions.c
+++ programs/functionLibraryAutoProgramQuestions.c
@@ -1,11 +1,48 @@
-#define nxtorange 3
-#define nxtleft 2
-#define nxtright 1
-#define nxtgrey 0
+#include "functionLibraryCommon.c";
 
+bool RPGtime = NULL;
 bool RPGstart = NULL;
-
+bool RPGmove30 = NULL;
+bool RPGmove60 = NULL;
+bool RPGmove90 = NULL;
+bool RPGscore30 = NULL;
+bool RPGscore60 = NULL;
+bool RPGscore90 = NULL;
+bool RPGscoreCenter = NULL;
+bool RPGkickStand = NULL;
+/**********************************************************
+1st Question
+***********************************************************/
 bool Question1()
+{
+	nxtDisplayCenteredTextLine(1, "do you want to wait ten seconds");//Displays second question
+	nxtDisplayCenteredTextLine(4, "left = no."); // Displays instructions
+	nxtDisplayCenteredTextLine(3, " right = yes."); // Displays instructions
+
+	while(RPGtime == NULL)
+	{
+		if(nNxtButtonPressed == nxtright)// checks if the right btn is pushed
+		{
+			// sets RPGtime to true
+			nxtDisplayClearTextLine(1);//clears the question
+			RPGtime = true;
+			return true;
+		}
+
+		if(nNxtButtonPressed == nxtleft)// checks if the left btn is pushed
+		{
+			// sets RPGtime to false
+			nxtDisplayClearTextLine(1);//clears the question
+			RPGtime = false;
+			return false;
+		}
+	}
+	return true;
+}
+/**********************************************************
+2nd Question
+***********************************************************/
+bool Question2()
 {
 	nxtDisplayCenteredTextLine(4, "left = floor."); // Displays instructions
 	nxtDisplayCenteredTextLine(3, " right = ramp."); // Displays instructions
@@ -17,63 +54,38 @@ bool Question1()
 		{
 			// sets RPGtime to true
 			nxtDisplayClearTextLine(1);//clears the question
-			nxtDisplayClearTextLine(3);
-			nxtDisplayClearTextLine(4);
-			RPGtime = true;
-			return true;
+			nxtDisplayClearTextLine(3);//clears the instructions for next set of instructions
+			nxtDisplayClearTextLine(4);//clears the instructions for next set of instructions
+			RPGstart = true;
+			return true;//returns true
 		}
 
 		if(nNxtButtonPressed == nxtleft)// checks if the left btn is pushed
 		{
 			// sets RPGtime to false
 			nxtDisplayClearTextLine(1);//clears the question
-			RPGtime = false;
-			return false;
+			RPGstart = true;
+			return false;//returns false
 		}
 	}
 	return true;
 }
-
-bool Question2()
-{
-	nxtDisplayCenteredTextLine(1, "do the kick stand");//Displays second question
-	nxtDisplayCenteredTextLine(4, "left = no."); // Displays instructions
-	nxtDisplayCenteredTextLine(3, " right = yes."); // Displays instructions
-
-	while(RPGtime == NULL)
-	{
-		if(nNxtButtonPressed == nxtright)// checks if the right btn is pushed
-		{
-			// sets RPGtime to true
-			nxtDisplayClearTextLine(1);//clears the question
-			RPGtime = true;
-			return true;
-		}
-
-		if(nNxtButtonPressed == nxtleft)// checks if the left btn is pushed
-		{
-			// sets RPGtime to false
-			nxtDisplayClearTextLine(1);//clears the question
-			RPGtime = false;
-			return false;
-		}
-	}
-	return true;
-}
-
-bool Question3()
+/**********************************************************
+4th Question
+***********************************************************/
+bool Question4()
 {
 	nxtDisplayCenteredTextLine(1, "move 30cm tube?");//Displays second question
 	nxtDisplayCenteredTextLine(4, "left = no."); // Displays instructions
 	nxtDisplayCenteredTextLine(3, " right = yes."); // Displays instructions
 
-	while(RPGtime == NULL)
+	while(RPGmove30 == NULL)
 	{
 		if(nNxtButtonPressed == nxtright)// checks if the right btn is pushed
 		{
 			// sets RPGtime to true
 			nxtDisplayClearTextLine(1);//clears the question
-			RPGtime = true;
+			RPGmove30 = true;
 			return true;
 		}
 
@@ -81,26 +93,28 @@ bool Question3()
 		{
 			// sets RPGtime to false
 			nxtDisplayClearTextLine(1);//clears the question
-			RPGtime = false;
+			RPGmove30 = false;
 			return false;
 		}
 	}
 	return true;
 }
-
-bool Question4()
+/**********************************************************
+5th Question
+***********************************************************/
+bool Question5()
 {
 	nxtDisplayCenteredTextLine(1, "move 60cm tube?");//Displays second question
 	nxtDisplayCenteredTextLine(4, "left = no."); // Displays instructions
 	nxtDisplayCenteredTextLine(3, " right = yes."); // Displays instructions
 
-	while(RPGtime == NULL)
+	while(RPGmove60 == NULL)
 	{
 		if(nNxtButtonPressed == nxtright)// checks if the right btn is pushed
 		{
 			// sets RPGtime to true
 			nxtDisplayClearTextLine(1);//clears the question
-			RPGtime = true;
+			RPGmove60 = true;
 			return true;
 		}
 
@@ -108,26 +122,28 @@ bool Question4()
 		{
 			// sets RPGtime to false
 			nxtDisplayClearTextLine(1);//clears the question
-			RPGtime = false;
+			RPGmove60 = false;
 			return false;
 		}
 	}
 	return true;
 }
-
-bool Question4()
+/**********************************************************
+6th Question
+***********************************************************/
+bool Question6()
 {
 	nxtDisplayCenteredTextLine(1, "move 90cm tube?");//Displays second question
 	nxtDisplayCenteredTextLine(4, "left = no."); // Displays instructions
 	nxtDisplayCenteredTextLine(3, " right = yes."); // Displays instructions
 
-	while(RPGtime == NULL)
+	while(RPGmove90 == NULL)
 	{
 		if(nNxtButtonPressed == nxtright)// checks if the right btn is pushed
 		{
 			// sets RPGtime to true
 			nxtDisplayClearTextLine(1);//clears the question
-			RPGtime = true;
+			RPGmove90 = true;
 			return true;
 		}
 
@@ -135,26 +151,28 @@ bool Question4()
 		{
 			// sets RPGtime to false
 			nxtDisplayClearTextLine(1);//clears the question
-			RPGtime = false;
+			RPGmove90 = false;
 			return false;
 		}
 	}
 	return true;
 }
-
-bool Question5()
+/**********************************************************
+7th Question
+***********************************************************/
+bool Question7()
 {
 	nxtDisplayCenteredTextLine(1, "score all in 30cm tube?");//Displays second question
 	nxtDisplayCenteredTextLine(4, "left = no."); // Displays instructions
 	nxtDisplayCenteredTextLine(3, " right = yes."); // Displays instructions
 
-	while(RPGtime == NULL)
+	while(RPGscore30 == NULL)
 	{
 		if(nNxtButtonPressed == nxtright)// checks if the right btn is pushed
 		{
 			// sets RPGtime to true
 			nxtDisplayClearTextLine(1);//clears the question
-			RPGtime = true;
+			RPGscore30 = true;
 			return true;
 		}
 
@@ -162,26 +180,28 @@ bool Question5()
 		{
 			// sets RPGtime to false
 			nxtDisplayClearTextLine(1);//clears the question
-			RPGtime = false;
+			RPGscore30 = false;
 			return false;
 		}
 	}
 	return true;
 }
-
-bool Question6()
+/**********************************************************
+8th Question
+***********************************************************/
+bool Question8()
 {
 	nxtDisplayCenteredTextLine(1, "score all in 60cm tube?");//Displays second question
 	nxtDisplayCenteredTextLine(4, "left = no."); // Displays instructions
 	nxtDisplayCenteredTextLine(3, " right = yes."); // Displays instructions
 
-	while(RPGtime == NULL)
+	while(RPGscore60 == NULL)
 	{
 		if(nNxtButtonPressed == nxtright)// checks if the right btn is pushed
 		{
 			// sets RPGtime to true
 			nxtDisplayClearTextLine(1);//clears the question
-			RPGtime = true;
+			RPGscore60 = true;
 			return true;
 		}
 
@@ -189,25 +209,28 @@ bool Question6()
 		{
 			// sets RPGtime to false
 			nxtDisplayClearTextLine(1);//clears the question
-			RPGtime = false;
+			RPGscore60 = false;
 			return false;
 		}
 	}
 	return true;
 }
-bool Question7()
+/**********************************************************
+9th Question
+***********************************************************/
+bool Question9()
 {
 	nxtDisplayCenteredTextLine(1, "score all in 90cm tube?");//Displays second question
 	nxtDisplayCenteredTextLine(4, "left = no."); // Displays instructions
 	nxtDisplayCenteredTextLine(3, " right = yes."); // Displays instructions
 
-	while(RPGtime == NULL)
+	while(RPGscore90 == NULL)
 	{
 		if(nNxtButtonPressed == nxtright)// checks if the right btn is pushed
 		{
 			// sets RPGtime to true
 			nxtDisplayClearTextLine(1);//clears the question
-			RPGtime = true;
+			RPGscore90 = true;
 			return true;
 		}
 
@@ -215,26 +238,28 @@ bool Question7()
 		{
 			// sets RPGtime to false
 			nxtDisplayClearTextLine(1);//clears the question
-			RPGtime = false;
+			RPGscore90 = false;
 			return false;
 		}
 	}
 	return true;
 }
-
-bool Question8()
+/**********************************************************
+3rd Question
+***********************************************************/
+bool Question3()
 {
-	nxtDisplayCenteredTextLine(1, "score all in center tube?");//Displays second question
+	nxtDisplayCenteredTextLine(1, "kick the kickstand");//Displays second question
 	nxtDisplayCenteredTextLine(4, "left = no."); // Displays instructions
 	nxtDisplayCenteredTextLine(3, " right = yes."); // Displays instructions
 
-	while(RPGtime == NULL)
+	while(RPGkickStand == NULL)
 	{
 		if(nNxtButtonPressed == nxtright)// checks if the right btn is pushed
 		{
 			// sets RPGtime to true
 			nxtDisplayClearTextLine(1);//clears the question
-			RPGtime = true;
+			RPGkickStand = true;
 			return true;
 		}
 
@@ -242,7 +267,36 @@ bool Question8()
 		{
 			// sets RPGtime to false
 			nxtDisplayClearTextLine(1);//clears the question
-			RPGtime = false;
+			RPGkickStand = false;
+			return false;
+		}
+	}
+	return true;
+}
+/**********************************************************
+10th Question
+***********************************************************/
+bool Question10()
+{
+	nxtDisplayCenteredTextLine(1, "score all in center tube?");//Displays second question
+	nxtDisplayCenteredTextLine(4, "left = no."); // Displays instructions
+	nxtDisplayCenteredTextLine(3, " right = yes."); // Displays instructions
+
+	while(RPGscoreCenter == NULL)
+	{
+		if(nNxtButtonPressed == nxtright)// checks if the right btn is pushed
+		{
+			// sets RPGtime to true
+			nxtDisplayClearTextLine(1);//clears the question
+			RPGscoreCenter = true;
+			return true;
+		}
+
+		if(nNxtButtonPressed == nxtleft)// checks if the left btn is pushed
+		{
+			// sets RPGtime to false
+			nxtDisplayClearTextLine(1);//clears the question
+			RPGscoreCenter = false;
 			return false;
 		}
 	}
