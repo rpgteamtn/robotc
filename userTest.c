7b7d36e824b8fafb0986f6d47e73291735c84994
totusme <marybeth@rogerdavis.info>
2014-11-02 16:24:47 -0600

    pragma usertest

--- userTest.c
+++ userTest.c
@@ -1,4 +1,5 @@
 #pragma config(Hubs,  S1, HTMotor,  HTMotor,  none,     none)
+#pragma config(Sensor, S1,     ,               sensorI2CMuxController)
 #pragma config(Sensor, S3,     IR,             sensorI2CCustom)
 #pragma config(Sensor, S4,     gyro,           sensorI2CHiTechnicGyro)
 #pragma config(Motor,  mtr_S1_C1_1,     leftFront,     tmotorTetrix, openLoop, reversed, encoder)
